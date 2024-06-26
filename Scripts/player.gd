e417366c8a3459027e668f8e03095123661ab500
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-06-26 18:20:48 -0400

    Added player stuff and removed game.tscn

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
 @export var player_speed : float = 300; # Speed in pixels per second
+@export var gravity : float = 980; # Gravity in pixels per second squared
 
 func _physics_process (delta: float):
 
-    var movement_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+    var movement_x = Input.get_axis("move_left", "move_right")
 
     # Super basic :skull:, definetly will add more depth. Dash maybe?
 
-    velocity = movement_vector * player_speed
+    if not is_on_floor():
+        velocity.y += gravity * delta
+    else:
+        velocity.x = movement_x * player_speed
+        if Input.is_action_just_pressed("jump"):
+            velocity.y = -sqrt(2 * gravity * 100)
+
     move_and_slide()

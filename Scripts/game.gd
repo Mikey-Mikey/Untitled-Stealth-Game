00bbfc3014cc74829d98fc4df5371befa4594854
Delano Leslie <delanoleslieprof@gmail.com>
2024-06-26 15:48:16 -0700

    Update game script add seperate thing for testing

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -5,6 +5,8 @@ class_name Game
 static var player : Player = null
 static var instance: Game = null
 
+signal game_over()
+
 var suspicion_time : float = 0 # The time the player has been in the detection zone if this get bigger than some amount the player is detected
 var suspicion_time_max : float = 2 # The time the player has been in the detection zone
 var suspicion_decay : float = 0 # The time the player has been in the detection zone
@@ -16,3 +18,17 @@ func _exit_tree():
 func _ready():
     instance = self
     player = $Player
+
+func _process(delta):
+    print(suspicion_time)
+    suspicion_time -= suspicion_decay * delta
+    if suspicion_time < 0:
+        suspicion_time = 0
+
+    if suspicion_time > suspicion_time_max:
+        emit_signal("game_over")
+        _game_over()
+
+func _game_over():
+
+    print("Game Over")

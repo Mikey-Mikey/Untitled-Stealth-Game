c196036bfd2aebdcb0c0ade4130018bbe9b49076
Mikey <67529953+Mikey-Mikey@users.noreply.github.com>
2024-06-26 18:53:27 -0400

    Added wall jumping

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,13 +6,19 @@ class_name Player
 @export var gravity : float = 1200 # Gravity in pixels per second squared
 @export var player_sprite : AnimatedSprite2D = null
 
+@onready var left_wall_detect = $LeftWallDetect
+@onready var right_wall_detect = $RightWallDetect
+
 func _physics_process (delta: float):
 
     var movement_x = Input.get_axis("move_left", "move_right")
+
     if movement_x != 0:
+        player_sprite.flip_h = movement_x > 0
+
+    if abs(velocity.x) > 5:
         player_sprite.play("run")
         player_sprite.speed_scale = abs(velocity.x * 0.006)
-        player_sprite.flip_h = movement_x > 0
     elif is_on_floor():
         player_sprite.play("idle")
 
@@ -21,10 +27,18 @@ func _physics_process (delta: float):
     if not is_on_floor():
         player_sprite.play("jump")
         velocity.y += gravity * delta
-        velocity.x += movement_x * player_speed * delta
+        velocity.x += (movement_x * player_speed - velocity.x) * 0.05
     else:
-        velocity.x += (movement_x * player_speed - velocity.x) * 0.1
-        if Input.is_action_just_pressed("jump"):
+        velocity.x += (movement_x * player_speed - velocity.x) * 0.2
+
+    if Input.is_action_just_pressed("jump"):
+        if is_on_floor() or left_wall_detect.has_overlapping_bodies() or right_wall_detect.has_overlapping_bodies():
             velocity.y = -sqrt(2 * gravity * 100)
+        
+        if left_wall_detect.has_overlapping_bodies():
+            velocity.x = player_speed
+        
+        if right_wall_detect.has_overlapping_bodies():
+            velocity.x = -player_speed
 
     move_and_slide()
